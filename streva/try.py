84aa80f3ac74cb5aeaadaa4efebea13a547dd3a5
Tomas Marsalek <marsalek.tomas@ifortuna.cz>
2015-06-07 00:55:26 +0200

    Yoyo

--- streva/try.py
+++ streva/try.py
@@ -1,73 +1,104 @@
 #!/usr/bin/env python3
 
+from streva.actor import MeasuredMixin, SupervisorMixin, Actor
+from streva.reactor import Reactor
 import logging
-
-from streva.reactor import *
-from streva.actor import *
-
-#
-# Basic test so that this module can be tested immediately
-class Counter(SupervisorMixin, Actor):
-    """ Sample implementation of Actor which generates sequence of numbers
-    in periodic intervals and sends them out for printing.
-    """
-
-    def __init__(self, reactor, count_from, name):
-        super().__init__(reactor=reactor, name=name, timeout_period=.1, probe_period=.5)
-        self.out_port = self.make_port("count")
-        self.count = count_from
-
-    def init(self, message):
-        def cb(_):
-            self.out_port.send(self.count)
-            self.count += 1
-
-            # Loop pattern
-            self.add_timeout(cb, .01)
-        self.add_timeout(cb, .01)
+import signal
+import threading
 
 
-class Printer(MeasuredMixin, Actor):
-    """ Sample implementation of Actor which simply prints numbers received
-    from Counter.
-    """
+# Synchronize by locking (or sending a message metaphorically) between reactor
+# threads and main thread
+wait = threading.Lock()
+
+
+class StopProduction(Exception):
+    pass
+
+
+class Producer(MeasuredMixin, Actor):
 
     def __init__(self, reactor, name):
         super().__init__(reactor=reactor, name=name)
-        self.add_handler("print", self.on_print)
+        self.out = self.make_port("out")
+        self.count = 1
 
-    def on_print(self, count):
-        logging.info("Printing " + str(count))
+    def init(self, _):
+        self.add_timeout(self.produce, .00001)
 
-
-def test():
-    # Define engines
-    reactor = Reactor()
-    io_reactor = IOReactor()
-
-    # Define logical components
-    counter = Counter(reactor, 1, "counter")
-    printer = Printer(io_reactor, "printer")
-
-    counter.supervise(printer)
-
-    # Wire components together.
-    # eg. subscribe 'printer.print' to 'counter.count'
-    counter.connect("count", printer, "print")
+    def produce(self, msg):
+        self.out.send(self.count)
+        self.count += 1
+        self.add_timeout(self.produce, .01)
 
 
-    # Set up logging
-    logging.basicConfig(format="%(levelname)s -- %(message)s",
-                        level=logging.INFO)
+class Consumer(MeasuredMixin, Actor):
 
-    # Register all components within supervisor and start them
-    supervisor = Emperor()
-    supervisor.add(reactor)
-    supervisor.add(io_reactor)
+    def __init__(self, reactor, name):
+        super().__init__(reactor=reactor, name=name)
+        self.add_handler("in", self.on_receive)
 
-    supervisor.start_all()
+    def on_receive(self, msg):
+        logging.info("Count is: {}".format(msg))
+
+
+class Supervisor(SupervisorMixin, Actor):
+
+    def __init__(self, reactor, name):
+        super().__init__(reactor=reactor, name=name, timeout_period=.5, probe_period=2)
+        self.stopped = False
+
+    def error_received(self, err):
+        errored_event, error = err
+        logging.exception(err)
+        self.finish(None)
+
+    def finish(self, _):
+        if not self.stopped:
+            self.stop_supervised()
+            self.stop()
+
+            for actor in self.get_supervised():
+                try:
+                    actor.print_stats()
+                except AttributeError:
+                    pass
+
+            self.stopped = True
+            wait.release()
+
+
+def register_stop_signal(supervisor):
+    def signal_stop_handler(sig, frame):
+        supervisor.finish(None)
+
+    for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGHUP):
+        signal.signal(sig, signal_stop_handler)
 
 
 if __name__ == "__main__":
-    test()
+    reactor = Reactor()
+
+    # Define actors
+    producer = Producer(reactor, "producer")
+    consumer = Consumer(reactor, "consumer")
+    supervisor = Supervisor(reactor, "supervisor")
+
+    producer.connect("out", consumer, "in")
+    supervisor.supervise(producer)
+    supervisor.supervise(consumer)
+
+    # Register keyinterrupt signals to be effective
+    register_stop_signal(supervisor)
+
+    # Configure logging
+    logging.basicConfig(format="%(levelname)s -- %(message)s", level=logging.INFO)
+
+    # Start the reactor
+    wait.acquire()
+    reactor.start()
+
+    # Wait for end signal from root supervisor and end the reactor
+    wait.acquire()
+    reactor.stop()
 
