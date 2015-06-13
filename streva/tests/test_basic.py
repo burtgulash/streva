a8fa190fd8c2e335aa188d842f459f0d1d2030e5
burtgulash <devnull@localhost>
2015-06-13 11:28:41 +0200

    Fix test after restructure.

--- streva/tests/test_basic.py
+++ streva/tests/test_basic.py
@@ -1,8 +1,8 @@
 import queue
 
 import streva.reactor
-from streva.actor import MeasuredMixin, MonitoredMixin, SupervisorMixin, Actor
-from streva.reactor import TimedLoop, Emperor
+from streva.actor import DelayableMixin, TimerMixin, MeasuredMixin, MonitoredMixin, SupervisorMixin, Actor
+from streva.reactor import Loop, TimedLoop, Emperor
 
 
 MARGINAL_DELAY = .000001
@@ -12,18 +12,19 @@ class StopProduction(Exception):
     pass
 
 
-class Producer(MonitoredMixin, Actor):
+class Producer(MonitoredMixin, DelayableMixin, Actor):
 
     def __init__(self, loop, name):
         super().__init__(loop, name)
+        self.add_handler("produce", self.produce)
         self.out = self.make_port("out")
         self.count = 1
 
     def init(self):
-        self.add_timeout(self.produce, MARGINAL_DELAY)
+        self.delay("produce", MARGINAL_DELAY)
 
     def produce(self, msg):
-        self.add_timeout(self.produce, MARGINAL_DELAY)
+        self.delay("produce", MARGINAL_DELAY)
         self.out.send(self.count)
         self.count += 1
 
@@ -39,11 +40,12 @@ class Consumer(MonitoredMixin, Actor):
             raise StopProduction
 
 
-class Supervisor(SupervisorMixin, Actor):
+class Supervisor(SupervisorMixin, TimerMixin, Actor):
 
-    def __init__(self, loop, name):
+    def __init__(self, loop, name, emperor):
         super().__init__(loop, name, timeout_period=.1, probe_period=.5)
         self.stopped = False
+        self.emperor = emperor
 
     def error_received(self, error_context):
         error = error_context.get_exception()
@@ -52,25 +54,31 @@ class Supervisor(SupervisorMixin, Actor):
             self.stop_children()
 
     def all_stopped(self, _):
+        self.emperor.stop()
         self.stop()
-        self.get_loop().stop()
 
 
 
 def test_count_to_100():
-    loop = TimedLoop()
+    loop = Loop()
+    timer_loop = TimedLoop()
+
+    emp = Emperor()
+    emp.add_loop(loop)
+    emp.add_loop(timer_loop)
 
     # Define actors
     producer = Producer(loop, "producer")
     consumer = Consumer(loop, "consumer")
-    supervisor = Supervisor(loop, "supervisor")
+    supervisor = Supervisor(timer_loop, "supervisor", emp)
 
     producer.connect("out", consumer, "in")
     supervisor.supervise(producer)
     supervisor.supervise(consumer)
 
-    emp = Emperor()
-    emp.add_loop(loop)
+    producer.connect_timer(supervisor)
+    supervisor.connect_timer(supervisor)
+
 
     emp.start()
     emp.join()
